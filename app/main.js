4c5b2ebbecaa80d29a4a6b6f14bc3c2889e5e9e1
Krunal Lathiya <KRUNAL@DESKTOP-51BTMB4.(none)>
2017-03-29 00:26:51 +0530

    ES6 Environment

---
+++ app/main.js
@@ -0,0 +1,4 @@
+let app = (name) => {
+    console.log(`hello from ${name}`);
+}
+app('Krunal');
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: './app/main.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }
+        ]
+    },
+    devServer: {
+        port: 3000,
+        inline: true
+    }
+};
